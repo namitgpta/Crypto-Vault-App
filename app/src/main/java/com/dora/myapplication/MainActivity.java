3762dc973fbe43bdc5c06d874ea9ba54c54043db
namitgpta <namitg677@gmail.com>
2022-01-26 01:14:03 +0530

    Added github repo button

--- app/src/main/java/com/dora/myapplication/MainActivity.java
+++ app/src/main/java/com/dora/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.dora.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +12,7 @@ import com.dora.myapplication.ImageSteganography.ImgSteg1;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button imgStegBtn;
+    Button imgStegBtn, gitHubRepoBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +20,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imgStegBtn = findViewById(R.id.imgStegBtn);
-        imgStegBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, ImgSteg1.class));
-            }
-        });
+        gitHubRepoBtn = findViewById(R.id.githubRepoBtn);
+
+        imgStegBtn.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, ImgSteg1.class)));
+        gitHubRepoBtn.setOnClickListener(view -> gitHubRepoExternalRedirect());
+
     }
+
+    private void gitHubRepoExternalRedirect() {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/namitgpta/Crypto-Vault-App")));
+    }
+
+
 }
