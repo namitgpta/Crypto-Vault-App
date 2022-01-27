8b9a7ee7344120cd9b93bc3a53f40c8218ef2213
namitgpta <namitg677@gmail.com>
2022-01-28 01:56:47 +0530

    Initialized section for Text Cryptography

--- app/src/main/java/com/dora/myapplication/Cryptography/CryptographyMainActivity.java
+++ app/src/main/java/com/dora/myapplication/Cryptography/CryptographyMainActivity.java
@@ -2,15 +2,24 @@ package com.dora.myapplication.Cryptography;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
+import com.dora.myapplication.Cryptography.Symmetric.SymmetricMainActivity;
 import com.dora.myapplication.R;
 
 public class CryptographyMainActivity extends AppCompatActivity {
+
+    Button symmCiphersBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cryptography_main);
+
+        symmCiphersBtn = findViewById(R.id.symmCiphersBtn);
+
+        symmCiphersBtn.setOnClickListener(view -> startActivity(new Intent(CryptographyMainActivity.this, SymmetricMainActivity.class)));
     }
 }
---
+++ app/src/main/java/com/dora/myapplication/Cryptography/Symmetric/SymmetricMainActivity.java
@@ -0,0 +1,16 @@
+package com.dora.myapplication.Cryptography.Symmetric;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dora.myapplication.R;
+
+public class SymmetricMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_symmetric_main);
+    }
+}
