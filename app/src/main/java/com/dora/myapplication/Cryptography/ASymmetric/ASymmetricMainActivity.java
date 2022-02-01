10cb0b337c65a3e0f6826cd7d29980ba599f7e84
namitgpta <namitg677@gmail.com>
2022-02-01 12:23:54 +0530

    Initialized section for Text Cryptography

---
+++ app/src/main/java/com/dora/myapplication/Cryptography/ASymmetric/ASymmetricMainActivity.java
@@ -0,0 +1,16 @@
+package com.dora.myapplication.Cryptography.ASymmetric;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dora.myapplication.R;
+
+public class ASymmetricMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asymmetric_main);
+    }
+}
